022a7d60b920de02b606083e916b326bd9989f11
付鹏凯 <pengkai.fu@dmall.com>
2019-02-20 14:56:12 +0800

    change dependency module

--- initializr-generator-spring/src/main/java/io/spring/initializr/generator/spring/build/maven/BaseChildDependencyMavenBuildCustomizer.java
+++ initializr-generator-spring/src/main/java/io/spring/initializr/generator/spring/build/maven/BaseChildDependencyMavenBuildCustomizer.java
@@ -5,8 +5,6 @@ import io.spring.initializr.generator.buildsystem.DependencyContainer;
 import io.spring.initializr.generator.buildsystem.maven.MavenBuild;
 import io.spring.initializr.generator.project.module.DefaultModuleTopology;
 import io.spring.initializr.generator.spring.build.BuildCustomizer;
-import io.spring.initializr.generator.spring.build.MetadataBuildItemMapper;
-import io.spring.initializr.metadata.Dependency;
 import org.springframework.util.StringUtils;
 
 import java.util.List;
@@ -19,24 +17,23 @@ import java.util.stream.Collectors;
 public class BaseChildDependencyMavenBuildCustomizer implements BuildCustomizer<MavenBuild> {
 
 
-    static final String DEFAULT_STARTER = "root_starter";
-    public static final String ORG_SPRINGFRAMEWORK_BOOT = "org.springframework.boot";
+    private static final String ORG_SPRINGFRAMEWORK_BOOT = "org.springframework.boot";
 
 
     // extension here
     @Override
     public void customize(MavenBuild build) {
-        build.getChildBuilds().stream()
-                .filter(mavenBuild -> mavenBuild.getArtifact().equals(DefaultModuleTopology.Modules.COMMON.getName()))
-                .findFirst()
-                .ifPresent(this::addDefaultStarterDependency);
-
         DependencyContainer dependencies = build.dependencies();
         List<io.spring.initializr.generator.buildsystem.Dependency> filteredDependencies = filterModuleDependency(dependencies);
 
         for (MavenBuild childBuild : build.getChildBuilds()) {
             for (io.spring.initializr.generator.buildsystem.Dependency dependency : filteredDependencies) {
                 if (dependency.getModule().equals(childBuild.getArtifact())) {
+                    childBuild.dependencies()
+                            .add(dependency.getId(), dependency);
+                } else if (null == dependency.getModule() && DefaultModuleTopology.Modules.COMMON.getName()
+                        .equals(childBuild.getArtifact())) {
+                    // default behavior, set the dependency to common module
                     childBuild.dependencies()
                             .add(dependency.getId(), dependency);
                 }
@@ -65,18 +62,6 @@ public class BaseChildDependencyMavenBuildCustomizer implements BuildCustomizer<
         return dependencies.items()
                 .filter(dependency -> !StringUtils.isEmpty(dependency.getModule()))
                 .collect(Collectors.toList());
-    }
-
-
-    private void addDefaultStarterDependency(MavenBuild mavenBuild) {
-        DependencyContainer dependencies = mavenBuild.dependencies();
-
-        // spring-boot-starter
-        io.spring.initializr.metadata.Dependency root = new Dependency();
-        root.setId(DEFAULT_STARTER);
-        root.asSpringBootStarter("");
-        dependencies.add(DEFAULT_STARTER,
-                MetadataBuildItemMapper.toDependency(root));
     }
 
 
